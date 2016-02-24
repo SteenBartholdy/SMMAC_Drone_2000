d14764fe469306e5a23b56a0e187db8817924d88
Roos <s145094@student.dtu.dk>
2016-02-24 10:13:02 +0100

    battery

--- SMMAC_Dron_2000/src/Commands.java
+++ SMMAC_Dron_2000/src/Commands.java
@@ -1,23 +1,38 @@
 import de.yadrone.base.command.LEDAnimation;
+import de.yadrone.base.navdata.BatteryListener;
 import de.yadrone.base.IARDrone;
 import de.yadrone.base.command.CommandManager;
 
 
 public class Commands {
 
-private CommandManager cmd;
-	
+	private CommandManager cmd;
+	private IARDrone drone;
+
 	public Commands(IARDrone drone)
 	{
-		cmd = drone.getCommandManager();
+		this.cmd = drone.getCommandManager();
+		this.drone = drone;
 	}
 
 	public void ledAnimationOrangeBlink()
 	{
 		cmd.setLedsAnimation(LEDAnimation.BLINK_ORANGE, 3, 10);
 	}
-	
+
 	public boolean wifi(){
 		return cmd.isConnected();
+	}
+
+	public void printBattery(){
+		drone.getNavDataManager().addBatteryListener(new BatteryListener() {
+
+			public void batteryLevelChanged(int percentage)
+			{
+				System.out.println("Battery: " + percentage + " %");
+			}
+
+			public void voltageChanged(int vbat_raw) { }
+		});
 	}
 }
