360ce0a19a9a2c451af2aa4313630cb57f8a207b
SteenBartholdy <s145090@student.dtu.dk>
2016-03-30 10:27:38 +0200

    IP

--- SMMAC_Dron_2000/src/Drone/ImageProcessor.java
+++ SMMAC_Dron_2000/src/Drone/ImageProcessor.java
@@ -4,6 +4,7 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -49,13 +50,22 @@ public class ImageProcessor {
 		}
 		int bufferSize = matrix.channels()*matrix.cols()*matrix.rows();
 		byte [] buffer = new byte[bufferSize];
-		matrix.get(0,0,buffer); // get all the pixels
+		matrix.get(0,0,buffer); 
 		BufferedImage image = new BufferedImage(matrix.cols(),matrix.
 				rows(), type);
 		final byte[] targetPixels = ((DataBufferByte) image.getRaster().
 				getDataBuffer()).getData();
 		System.arraycopy(buffer, 0, targetPixels, 0, buffer.length);
 		return image;
+	}
+	
+	public Mat toMatImage(BufferedImage image) {
+		
+		byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		Mat imageMat = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3);
+		imageMat.put(0, 0, pixels);
+
+		return imageMat;
 	}
 
 
