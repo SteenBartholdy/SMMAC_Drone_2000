d7941cf68960cb253ef0df3335ed74cd5e957605
Roos <s145094@student.dtu.dk>
2016-06-03 14:23:04 +0200

    mod

--- SMMAC_Dron_2000/src/Test/OpticalTest.java
+++ SMMAC_Dron_2000/src/Test/OpticalTest.java
@@ -3,17 +3,13 @@ package Test;
 import org.opencv.core.Core;
 import org.opencv.imgcodecs.Imgcodecs;
 
-import Drone.OpticalFlow;
-
 public class OpticalTest {
 	
 	public static void main(String[] args) {
 		
 		OpticalFlow op = new OpticalFlow();
 		
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		op.useOpticalFlow(Imgcodecs.imread("/Users/Christoffer/Pictures/b1.png"), Imgcodecs.imread("/Users/Christoffer/Pictures/b2.png"));
-		
+		op.run();
 	}
 
 }
