6a5cb4def45b971573988c7e306cb46aafabd64f
Roos <s145094@student.dtu.dk>
2016-06-06 08:54:11 +0200

    OP

--- SMMAC_Dron_2000/src/Drone/ImageProcessor.java
+++ SMMAC_Dron_2000/src/Drone/ImageProcessor.java
@@ -19,6 +19,8 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.video.Video;
 
 public class ImageProcessor {
+	
+	private double THRESHOLD = 40;
 
 	public Mat erode(Mat input, int elementSize, int elementShape) {
 		Mat outputImage = new Mat();
@@ -95,17 +97,22 @@ public class ImageProcessor {
 	}
 
 	public void useOpticalFlow (Mat imagePrev, Mat imageNext) {
-		Mat grayImagePrev = new Mat();
-		Mat grayImageNext = new Mat();
+		Mat processedImagePrev = new Mat();
+		Mat processedImageNext = new Mat();
 		MatOfByte status = new MatOfByte();
 		MatOfFloat err = new MatOfFloat();
 		MatOfPoint pointsPrev = new MatOfPoint();
-		Imgproc.cvtColor(imagePrev, grayImagePrev, Imgproc.COLOR_BGR2GRAY);
-		Imgproc.goodFeaturesToTrack(grayImagePrev, pointsPrev, 100, 0.01, 1);
-		Imgproc.cvtColor(imageNext, grayImageNext, Imgproc.COLOR_BGR2GRAY);
+		
+		Imgproc.cvtColor(imagePrev, processedImagePrev, Imgproc.COLOR_BGR2GRAY);
+		Imgproc.Canny(processedImagePrev, processedImagePrev, THRESHOLD, THRESHOLD*2);
+		Imgproc.goodFeaturesToTrack(processedImagePrev, pointsPrev, 100, 0.1, 1);
+		
+		Imgproc.cvtColor(imageNext, processedImageNext, Imgproc.COLOR_BGR2GRAY);
+		Imgproc.Canny(processedImageNext, processedImageNext, THRESHOLD, THRESHOLD*2);
+		
 		MatOfPoint2f c1 = new MatOfPoint2f(pointsPrev.toArray());
 		MatOfPoint2f c2 = new MatOfPoint2f();
-		Video.calcOpticalFlowPyrLK(grayImagePrev, grayImageNext, c1, c2, status, err);
+		Video.calcOpticalFlowPyrLK(processedImagePrev, processedImageNext, c1, c2, status, err);
 
 		for (int i = 0; i < status.rows(); i++) {
 			int statusInt = (int) status.get(i, 0)[0];
@@ -113,7 +120,7 @@ public class ImageProcessor {
 				double[] cornerPoints1 = c1.get(i, 0);
 				double[] cornerPoints2 = c2.get(i, 0);
 				Imgproc.line(imageNext, new Point(cornerPoints1[0], cornerPoints1[1]), 
-						new Point(cornerPoints2[0], cornerPoints2[1]), new Scalar(255,50,0), 2);
+						new Point(cornerPoints2[0], cornerPoints2[1]), new Scalar(233,121,255), 2);
 			}
 		}
 	}
@@ -126,7 +133,7 @@ public class ImageProcessor {
 
 		Imgproc.cvtColor(image, greyImage, Imgproc.COLOR_BGR2GRAY);
 		Imgproc.GaussianBlur(greyImage, blurImage, new Size(3,3), 2,2);
-		Imgproc.HoughCircles(blurImage, circleImage, Imgproc.CV_HOUGH_GRADIENT, 1, 1000, 40, 40*2, 80, 500);
+		Imgproc.HoughCircles(blurImage, circleImage, Imgproc.CV_HOUGH_GRADIENT, 1, 1000, THRESHOLD, THRESHOLD*2, 80, 500);
 
 		for(int i = 0; i < circleImage.cols(); i++)
 		{
