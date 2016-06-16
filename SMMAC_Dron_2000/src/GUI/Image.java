414b6731ad04a06089bfe5f58a6ed27b3ca52345
Anders TT <s140996@student.dtu.dk>
2016-06-16 11:14:40 +0200

    removeFish()

--- SMMAC_Dron_2000/src/GUI/Image.java
+++ SMMAC_Dron_2000/src/GUI/Image.java
@@ -11,6 +11,7 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
 import org.opencv.core.Point;
 
 import de.yadrone.base.video.ImageListener;
@@ -61,4 +62,31 @@ public class Image extends JFrame implements ImageListener {
 		}
 	}
 
+	public void removeFish()
+	{
+		Mat cMatrix = new Mat(3, 3, 5);
+		printMatrixMat(cMatrix);
+		cMatrix.put(0, 0, 1.1333e03);
+		cMatrix.put(0, 1, 0.0);
+		cMatrix.put(0, 2, 670.4082);
+		cMatrix.put(1, 0, 0.0);
+		cMatrix.put(1, 1, 1.1344e03);
+		cMatrix.put(1, 2, 297.8410);
+		cMatrix.put(2, 0, 0.0);
+		cMatrix.put(2, 1, 0.0);
+		cMatrix.put(2, 2, 1);
+		printMatrixMat(cMatrix);
+		Mat dCoef = new Mat(1, 4, 5);
+		dCoef.put(0, 0, -0.5719);
+		dCoef.put(0, 1, 0,3844);
+		dCoef.put(0, 2, 0);
+		dCoef.put(0, 3, 0);
+		printMatrixMat(dCoef);
+	}
+	
+	public void printMatrixMat(Mat matrixMat)
+	{
+		System.out.println("MatMatrix: " + matrixMat.dump());
+	}
+	
 }
